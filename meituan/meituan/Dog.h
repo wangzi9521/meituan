82d3ed07d50e54502c128e2c583b7adaf9ab7fb8
why <why@163.com>
2015-08-29 15:50:14 +0800

    创建了Dog类

---
+++ meituan/meituan/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  meituan
+//
+//  Created by xiaomage on 15/8/29.
+//  Copyright (c) 2015年 xiaomage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ meituan/meituan/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  meituan
+//
+//  Created by xiaomage on 15/8/29.
+//  Copyright (c) 2015年 xiaomage. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
