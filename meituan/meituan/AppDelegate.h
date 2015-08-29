2c23d02b395ea6b87df9d908bd1e637e4e1fbe29
why <why@163.com>
2015-08-29 15:48:19 +0800

    初始化项目

---
+++ meituan/meituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meituan
+//
+//  Created by xiaomage on 15/8/29.
+//  Copyright (c) 2015年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
